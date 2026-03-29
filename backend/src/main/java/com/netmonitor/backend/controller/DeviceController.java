bf67034181426429fff42851e9bfe631de8a327d
Pandiaraj-1 <pandiaraj011@gmail.com>
2026-03-29 15:54:32 +0530

    done some changes in images

--- backend/src/main/java/com/netmonitor/backend/controller/DeviceController.java
+++ backend/src/main/java/com/netmonitor/backend/controller/DeviceController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/devices")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class DeviceController {
 
     private final DeviceService deviceService;
